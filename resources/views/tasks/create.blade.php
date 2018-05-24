6be8b2be5b8ed8a0ff3e7f0683da8bd4730c3264
ayame.murayama <ayame.murayama@rakuten.com>
2018-05-24 01:18:08 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>タスク新規入力ページ</h1>
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('入力') !!}
+
+    {!! Form::close() !!}
+
+@endsection

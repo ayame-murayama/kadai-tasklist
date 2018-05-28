7c7f21dc8c998192661cdfbb5d0848167ff08db0
ayame.murayama <ayame.murayama@rakuten.com>
2018-05-28 01:51:18 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,18 +4,26 @@
 
 <h1>タスク新規入力ページ</h1>
 
+<div class="row">
+    <div class="col-xs-12","col-sm-offset2 col-sm=8","col-md-offset2 col-md-8","col-lg-offset3 col-lg-6">
 
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
+      <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status',null,['class' => 'form-control']) !!}
+      </div>
 
+      <div>
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('入力') !!}
+        {!! Form::text('content',null, ['class' => 'form-control']) !!}
+      </div>
+      
+        {!! Form::submit('入力', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+    </div>
+</div>
 
 @endsection

3184fd5686574c2d875bcebb439a482987d16711
ayame.murayama <ayame.murayama@rakuten.com>
2018-05-28 05:07:06 +0000

    design repair2

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,7 +5,7 @@
 <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
-      <table class="table table-striped">
+      <table class="table table-hover">
             <thead>
                 <tr>
                     <th>id</th>
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -5,9 +5,9 @@
 <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
 <div class="row">
-    <div class="col-xs-12","col-sm-offset2 col-sm=8","col-md-offset2 col-md-8","col-lg-offset3 col-lg-6">
+    <div class="col-xs-12" "col-sm-offset2 col-sm=8" "col-md-offset2 col-md-8" "col-lg-offset3 col-lg-6">
 
-    <table class="table table-bordered">
+    <table class="table table-hover">
         <tr>
             <th>id</th>
             <td>{{ $task->id }}</td>
